7e3383ec2e1e2723748e73a8e1c42e5aa70189a9
Ana Clara Nunes Oliveira <anunes-o@c1r8p2.42sp.org.br>
2026-06-08 14:50:07 -0300

    adding mutex protection on print_death and print_all_eat

--- philo/src/monitor.c
+++ philo/src/monitor.c
@@ -6,7 +6,7 @@
 /*   By: anunes-o <anunes-o@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 13:58:08 by anunes-o          #+#    #+#             */
-/*   Updated: 2026/06/04 16:34:25 by anunes-o         ###   ########.fr       */
+/*   Updated: 2026/06/08 14:47:10 by anunes-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,15 +53,16 @@ void	print_death(t_data *data, int id)
 {
 	pthread_mutex_lock(&data->death_mutex);
 	data->simulation_over = 1;
-	pthread_mutex_unlock(&data->death_mutex);
+	pthread_mutex_lock(&data->print_mutex);
 	printf("[%ld] %d died\n", get_lapse_time(data->start_time), id);
+	pthread_mutex_unlock(&data->print_mutex);
 }
 
 void	print_all_eat(t_data *data)
 {
 	pthread_mutex_lock(&data->death_mutex);
 	data->simulation_over = 1;
-	pthread_mutex_unlock(&data->death_mutex);
 	printf("[%ld] All philosophers have eaten %d times\n",
 		get_lapse_time(data->start_time), data->must_eat_count);
+	pthread_mutex_unlock(&data->print_mutex);
 }
